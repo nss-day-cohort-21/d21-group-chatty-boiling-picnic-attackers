f677675a2bb1c06258cd7cf1943d5b7ee23d221a
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-07-31 13:12:58 -0500

    new messages now inserted into object with ID

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 var Chatty = (function () {
-	let messagesArr = [];
 	let messageFrame = document.getElementById('message-frame');
 	let messageInput = document.getElementById('message-input');
+	let messagesArr = [];
 
+	let idCount = messagesArr.length;
+	function messagesObj(id, message) {
+		this.id = id,
+		this.message = message
+	};
 	messageInput.addEventListener('keyup', function(e){
 		if (e.which === 13) {
 			Chatty.getInput();
@@ -13,14 +18,16 @@ var Chatty = (function () {
 	return {
 		getInput: function() {
 			let input = messageInput.value;
-			messagesArr.push(input);
+			let newMessage = new messagesObj(idCount, input);
+			messagesArr.push(newMessage);
+			idCount++;
 			console.log("messagesArr", messagesArr);
 		},
 		writeToPage: function () {
 			messageInput.value = '';
 			let newP = document.createElement('p');
 			newP.classList.add('message');
-			newP.innerText = messagesArr[messagesArr.length - 1];
+			newP.innerText = messagesArr[messagesArr.length - 1].message;
 			messageFrame.appendChild(newP);
 		}
 	};
