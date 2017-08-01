b20684f6d459505ed020b644bff136c204d60657
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-08-01 09:57:00 -0500

    delete button functionality added, function sends array and element ID to IIFE

--- js/message_delete.js
+++ js/message_delete.js
@@ -1,20 +1,11 @@
 console.log("message_delete.js");
 
 var Chatty = (function(messageDelete) {
-	let deleteBtns = document.getElementsByClassName('delete-btn');
-	console.log("deleteBtns", deleteBtns);
-	messageDelete.deleteMessage = function() {
-		for (let i = 0; i < deleteBtns.length; i++) {
-			deleteBtns[i].addEventListener('click', function () {
-				let id = this.parentElement.id;
-				let deleteItem = document.getElementById(id);
-				deleteItem.remove();
-			});
-		}
+
+	messageDelete.deleteFromArray = function(arrayInput, messageId) {
+		console.log("messageId:", messageId, "arrayInput:", arrayInput);
+
 	};
-
-
-
 
 	return messageDelete;
 
