3801ed7dee66c35a848223c48bf14c8607639f19
palmierie <ericmpalmieri@gmail.com>
2017-08-01 11:12:08 -0500

    workin on del

--- js/message_delete.js
+++ js/message_delete.js
@@ -4,9 +4,14 @@ var Chatty = (function(messageDelete) {
 
 	messageDelete.deleteFromArray = function(arrayInput, messageId) {
 		console.log("messageId:", messageId, "arrayInput:", arrayInput);
-		arrayInput.splice(function(messageId){
-			
-		},1);
+		arrayInput.splice(messageId,1);
+
+		for(var i = arrayInput.length - 1; i >= 0; i--) {
+    if(arrayInput[i] === messageId) {
+       arr.splice(i, 1);
+    }
+}
+
 	};
 
 	return messageDelete;
