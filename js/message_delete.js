c6e31629f9110f2f6b108242a733c3b76e53cd74
palmierie <ericmpalmieri@gmail.com>
2017-08-01 10:17:35 -0500

    fixing message del

--- js/message_delete.js
+++ js/message_delete.js
@@ -1,25 +1,13 @@
 console.log("message_delete.js");
 
 var Chatty = (function(messageDelete) {
-<<<<<<< HEAD
-	let deleteBtns = document.getElementsByClassName('delete-btn');
-	console.log("deleteBtns", deleteBtns);
-	messageDelete.deleteMessage = function() {
-		for (let i = 0; i < deleteBtns.length; i++) {
-			deleteBtns[i].addEventListener('click', function () {
-				let id = this.parentElement.id;
-				let deleteItem = document.getElementById(id);
-				deleteItem.remove();
-			});
-		}
-	}
-=======
 
 	messageDelete.deleteFromArray = function(arrayInput, messageId) {
 		console.log("messageId:", messageId, "arrayInput:", arrayInput);
-
+		arrayInput.splice(function(messageId){
+			
+		},1);
 	};
->>>>>>> 56820f488e4107f862bd3a6dd8ecf4175d8586e7
 
 	return messageDelete;
 
