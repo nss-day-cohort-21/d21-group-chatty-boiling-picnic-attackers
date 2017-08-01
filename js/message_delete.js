a42d6901975fed7942ea3ff51c08ef29a7fb8b6a
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-08-01 11:08:18 -0500

    added delete functions for array and dom

--- js/message_delete.js
+++ js/message_delete.js
@@ -2,9 +2,22 @@ console.log("message_delete.js");
 
 var Chatty = (function(messageDelete) {
 
-	messageDelete.deleteFromArray = function(arrayInput, messageId) {
-		console.log("messageId:", messageId, "arrayInput:", arrayInput);
+	messageDelete.deleteFromArrayAndDom = function(arrayInput, messageId, nodeToDelete) {
+		messageFrame = document.getElementById("message-frame");
+		console.log("messageId:", messageId, "nodeToDelete", nodeToDelete, "arrayInput:", arrayInput);
+		console.log("arrayInput.id", arrayInput.id);
 
+			arrayInput = arrayInput.filter(function( obj ) {
+  			return obj.id != messageId;
+			});
+
+			// console.log("result", result);
+
+		// arrayInput.splice(messageId, 1); 
+		messageFrame.removeChild(nodeToDelete);
+		console.log("arrayInput after delete:", arrayInput);
+
+		return arrayInput;
 	};
 
 	return messageDelete;
