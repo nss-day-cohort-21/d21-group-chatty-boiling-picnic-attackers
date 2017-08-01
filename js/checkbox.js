eaebfd8e91b433f582a8624d03e8edebaba84996
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-01 12:06:32 -0500

    put checkbox functions in an IIFE

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,27 +1,37 @@
-var theme = document.getElementById("dark-light");
-var fontSize = document.getElementById("font-size");
-var bodyHtml = document.querySelector("body");
-var headerHtml = document.getElementById("header");
-var clearBoardBtnHtml = document.getElementById("clear-board");
-var buttonHtml = document.getElementsByClassName("delete-btn");
+var Chatty = (function(obj){
 
-function themeToggle(element) {
-  console.log('theme checked', element.checked);
-  if (element.checked === true){
-    bodyHtml.classList.add("dark-theme-custom");
-    headerHtml.classList.add("dark-theme-custom");
-    clearBoardBtnHtml.classList.add("dark-theme-custom");
-  } else {
-    bodyHtml.classList.remove("dark-theme-custom");
-    headerHtml.classList.remove("dark-theme-custom");
-    clearBoardBtnHtml.classList.remove("dark-theme-custom");
-  }
-}
+  var theme = document.getElementById("dark-light");
+  var fontSize = document.getElementById("font-size");
+  var bodyHtml = document.querySelector("body");
+  var headerHtml = document.getElementById("header");
+  var clearBoardBtnHtml = document.getElementById("clear-board");
+  var buttonHtml = document.getElementsByClassName("delete-btn");
 
-function fontToggle(element) {
-  if(element.checked === true){
-    bodyHtml.classList.add("large-text-custom");
-  } else {
-    bodyHtml.classList.remove("large-text-custom");
+  obj.themeToggle = function (element) {
+    console.log('theme checked', element.checked);
+    if (element.checked === true){
+      bodyHtml.classList.add("dark-theme-custom");
+      headerHtml.classList.add("dark-theme-custom");
+      clearBoardBtnHtml.classList.add("dark-theme-custom");
+    } else {
+      bodyHtml.classList.remove("dark-theme-custom");
+      headerHtml.classList.remove("dark-theme-custom");
+      clearBoardBtnHtml.classList.remove("dark-theme-custom");
+    }
   }
-}
+  obj.fontToggle = function (element) {
+    if(element.checked === true){
+      bodyHtml.classList.add("large-text-custom");
+    } else {
+      bodyHtml.classList.remove("large-text-custom");
+    }
+  }
+
+  theme.addEventListener('change', function() {
+    Chatty.themeToggle(this);
+  });
+  fontSize.addEventListener('change', function() {
+    Chatty.fontToggle(this);
+  });
+  return obj;
+}(Chatty || {}));
