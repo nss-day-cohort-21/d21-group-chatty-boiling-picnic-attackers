c91d87fb0b20ee9c895125ff9a45e9d996f6ab94
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-04 23:42:36 -0500

    delete button functionality

--- js/dom.js
+++ js/dom.js
@@ -1,14 +1,20 @@
 var Chatty = (function(obj) {
 	obj.writeToPage = function(data) {
 		for (let i = 0; i < data.length; i++) {
-			console.log("data in loop is ", data);
-			let $message = $('<p>', {'class': 'message'});
+			let $message = $('<p>', {'id': data[i].id});
 			let $delBtn = $('<button>');
 			$delBtn.text('Delete');
+			$delBtn.attr('class', 'delete');
 			$message.text(data[i].message);
 			$message.append($delBtn);
 			$('#message-frame').append($message);
 		}
+		$('.delete').click(function() {
+				let p = this.parentNode;
+				Chatty.deleteArrItem(p.id);
+				p.remove();
+			});
 	};
 	return obj;
 }(Chatty || {}));
+
